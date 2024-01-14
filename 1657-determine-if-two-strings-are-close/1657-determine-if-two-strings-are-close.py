02f07e18fe76728d01df958d9cb733305c3767f9
01_Triton <106975419+HarshitWaldia@users.noreply.github.com>
2024-01-14 19:00:16 +0530

    Time: 201 ms (41.20%), Space: 18.5 MB (20.97%) - LeetHub

---
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
@@ -0,0 +1,21 @@
+class Solution:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        if len(word1) != len(word2):
+            return False
+        
+        frequency1 = [0] * 26
+        frequency2 = [0] * 26
+
+        for letter in word1:
+            frequency1[ord(letter)- ord('a')] += 1
+
+        for letter in word2:
+            frequency2[ord(letter)- ord('a')] += 1
+
+        for index in range (0,26):
+            if frequency1[index] == 0 and frequency2[index] != 0:
+                return False
+            if frequency1[index] != 0 and frequency2[index] == 0:
+                return False
+
+        return sorted(frequency1) == sorted(frequency2)
